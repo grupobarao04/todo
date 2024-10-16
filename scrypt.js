5a2e8e33ffd1dec1ee6dc04d450e1b1ee1a7a193
grupobarao04 <kauafreitasaguiarferitasaguiar@gmail.com>
2024-10-16 08:52:44 -0300

    alert bem vindo

---
+++ scrypt.js
@@ -0,0 +1,12 @@
+const novaTarefa = document.getElementById('novaTarefa');
+const btnAddTarefa = document.getElementById('btnAddTarefa');
+const Listatarefa = document.getElementById('listaTarefa');
+
+btnAddTarefa.addEventListener('click', function(){
+
+alert('bem vindo');
+
+
+
+
+});
