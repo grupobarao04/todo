71f4bdc67468e934f1cb8a77427ce570a89e368b
grupobarao04 <kauafreitasaguiarferitasaguiar@gmail.com>
2024-10-16 10:10:31 -0300

    logica

--- scrypt.js
+++ scrypt.js
@@ -3,10 +3,31 @@ const btnAddTarefa = document.getElementById('btnAddTarefa');
 const Listatarefa = document.getElementById('listaTarefa');
 
 btnAddTarefa.addEventListener('click', function(){
+    const tarefa = novaTarefa.Value;
+   
+     // se a tarefa for diferente de vazia execulta 
+    if(tarefa !=  " "){
 
-alert(nova Tarefa );
+    const li =  document.createElement('li');
+    
+    li.textContent = novaTarefa;
+    
+    const botaoRemover = document.createElement('button');
+    botaoRemover.textContent = "Remover";
+
+    botaoRemover.addEventListener('click', function(){
+        li.remove();
+    });
 
 
+    li.appendChild(botaoRemover);
+    Listatarefa.appendChild(li);
 
+    novaTarefa.value = "";
+    }
+    
+
+
+    
 
 });
